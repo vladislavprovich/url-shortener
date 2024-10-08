807ab0524b02874e700e04ab21b9b492fb8c741b
Vlad <123kkrvlad@gmail.com>
2024-10-09 02:31:50 +0300

    add url_service logic

--- internal/service/url_service.go
+++ internal/service/url_service.go
@@ -1,8 +1,13 @@
 package service
 
 import (
+	"fmt"
+	"github.com/google/uuid"
 	"github.com/vladislavprovich/url-shortener/internal/models"
 	"github.com/vladislavprovich/url-shortener/internal/repository"
+	"github.com/vladislavprovich/url-shortener/pkg/shortener"
+	"log"
+	"time"
 )
 
 type URLService interface {
@@ -21,22 +26,68 @@ func NewURLService(repo repository.URLRepository) URLService {
 }
 
 func (s urlService) CreateShortURL(req models.ShortenRequest) (string, error) {
-	//TODO implement me
+	if req.URL == "" {
+		return "", fmt.Errorf("no long URL")
+	}
+	short := shortener.GeneratorShortURL()
+	log.Println(short)
+	_, err := s.repo.GetURL(short)
+	if err == nil {
+		return "", fmt.Errorf("short URL already exists")
+	}
+
 	panic("implement me")
 }
 
 func (s urlService) GetOriginalURL(shortURL string) (string, error) {
-	//TODO implement me
+	if shortURL == "" {
+		return "", fmt.Errorf("no short URL")
+	}
+	originalUrl, err := s.repo.GetURL(shortURL)
+	if err != nil {
+		return "", err
+	}
+	log.Println(originalUrl)
 	panic("implement me")
 }
 
 func (s urlService) LogRedirect(shortURL, referrer string) error {
-	//TODO implement me
+	if shortURL == "" {
+		return fmt.Errorf("no short URL")
+	}
+	//TODO referrer delete...
+	if referrer == "" {
+		return fmt.Errorf("no referrer")
+	}
+
+	res, err := s.repo.GetStats(shortURL)
+	if err != nil {
+		return err
+	}
+	log.Println(res)
+
+	var id = uuid.NewString()
+
+	logEntry := models.RedirectLog{
+		ID:         id,
+		ShortURL:   shortURL,
+		Referrer:   &referrer,
+		AccessedAt: time.Now(),
+	}
+	err = s.repo.SaveRedirectLog(logEntry)
+	if err != nil {
+		return fmt.Errorf("error saving redirect log: %w", err)
+	}
 	panic("implement me")
+	return nil
 }
 
 func (s urlService) GetStats(shortURL string) (models.StatsResponce, error) {
-	//TODO implement me
+	status, err := s.repo.GetStats(shortURL)
+	if err != nil {
+		return models.StatsResponce{}, fmt.Errorf("error getting stats: %w", err)
+	}
+	log.Println(status)
 	panic("implement me")
 }
 
